658b0ceaa4870f787307eb29a40c8b034098086d
Juan Tavares <juantavares2006@hotmail.com>
2019-01-03 17:00:51 -0300

    Recebendo dados da api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
+    HttpClientModule,
     NgbModule.forRoot()
   ],
   providers: [],
--- src/app/core/services/random-user.service.ts
+++ src/app/core/services/random-user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RandomUserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsers() {
+    return this.http.get('https://randomuser.me/api/');
+  }
 }
