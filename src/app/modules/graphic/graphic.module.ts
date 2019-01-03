37b0ebd0e20fb2f9a41eb11c455d9f28eafc5311
Juan Tavares <juantavares2006@hotmail.com>
2019-01-02 23:02:29 -0300

    Criando página de gráficos

--- src/app/modules/graphic/graphic.module.ts
+++ src/app/modules/graphic/graphic.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GraphicPageComponent } from './pages/graphic-page/graphic-page.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [GraphicPageComponent]
 })
 export class GraphicModule { }
---
+++ src/app/modules/graphic/pages/graphic-page/graphic-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphic-page',
+  templateUrl: './graphic-page.component.html',
+  styleUrls: ['./graphic-page.component.css']
+})
+export class GraphicPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
