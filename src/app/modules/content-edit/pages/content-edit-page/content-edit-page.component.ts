60a6e690431dafc4abad724fa6f85bfbc9ae75b5
Juan Tavares <juantavares2006@hotmail.com>
2019-01-04 04:46:54 -0300

    Exibindo tinyMce na página

--- src/app/modules/content-edit/pages/content-edit-page/content-edit-page.component.ts
+++ src/app/modules/content-edit/pages/content-edit-page/content-edit-page.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { NgbModal, NgbModalRef, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-content-edit-page',
   templateUrl: './content-edit-page.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./content-edit-page.component.css']
 })
-export class ContentEditPageComponent implements OnInit {
+export class ContentEditPageComponent {
 
-  constructor() { }
+  tinyMceSettings = {
+    inline: false,
+    statusbar: false,
+    browser_spellcheck: true,
+    height: 320
+  };
+  modal: any;
 
   ngOnInit() {
   }
