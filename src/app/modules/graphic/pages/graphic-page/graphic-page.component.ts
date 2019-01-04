884049bbc88bb5bedafa4d7a899abf2737e4e02f
Juan Tavares <juantavares2006@hotmail.com>
2019-01-04 10:56:43 -0300

    Adicionando mais dois gráficos

--- src/app/modules/graphic/pages/graphic-page/graphic-page.component.ts
+++ src/app/modules/graphic/pages/graphic-page/graphic-page.component.ts
@@ -34,6 +34,57 @@ export class GraphicPageComponent implements OnInit {
     });
 
     chart.render();
+
+    const chart2 = new CanvasJS.Chart('chartContainer2', {
+      theme: 'light2',
+      animationEnabled: true,
+      title: {
+        text: 'Custos mensais'
+      },
+      data: [{
+        type: 'pie',
+        showInLegend: true,
+        toolTipContent: '<b>{name}</b>: ${y} (#percent%)',
+        indexLabel: '{name} - #percent%',
+        dataPoints: [
+          { y: 450, name: 'Marketing' },
+          { y: 800, name: 'Tecnologia' },
+          { y: 150, name: 'Infraestrutura' },
+          { y: 150, name: 'Material' },
+          { y: 250, name: 'Outros' }
+        ]
+      }]
+    });
+    chart2.render();
+
+    const chart3 = new CanvasJS.Chart('chartContainer3', {
+      animationEnabled: true,
+      theme: 'light2',
+      title: {
+        text: 'Usuários engajados / H'
+      },
+      axisY: {
+        includeZero: false
+      },
+      data: [{
+        type: 'line',
+        dataPoints: [
+          { y: 450 },
+          { y: 414 },
+          { y: 520 },
+          { y: 460 },
+          { y: 450 },
+          { y: 500 },
+          { y: 480 },
+          { y: 480 },
+          { y: 410 },
+          { y: 500 },
+          { y: 480 },
+          { y: 510 }
+        ]
+      }]
+    });
+    chart3.render();
   }
 
 }
