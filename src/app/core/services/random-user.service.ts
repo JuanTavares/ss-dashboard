69c052a3dca3955f47140cce8c37cdf691050883
Juan Tavares <juantavares2006@hotmail.com>
2019-01-04 06:24:53 -0300

    Recebendo dados da api

--- src/app/core/services/random-user.service.ts
+++ src/app/core/services/random-user.service.ts
@@ -14,6 +14,11 @@ export class RandomUserService {
 
   getHundredUsers() {
     return this.http
-    .get(API + 'results=101&inc=name,gender,email,picture,dob&noinfo&seed=ssdashboard');
+      .get(API + 'results=101&inc=name,gender,email,picture,dob&noinfo&seed=ssdashboard');
+  }
+
+  getList() {
+    return this.http
+      .get('https://randomapi.com/api/do1zgi4d?key=LA2T-HR42-B5CQ-E4O6&results=10&fmt=raw');
   }
 }
