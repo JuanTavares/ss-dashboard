aa5520d7b1c99cc1f2de608ec3f3d62ace7fc009
Juan Tavares <juantavares2006@hotmail.com>
2019-01-04 06:25:36 -0300

    Aplicando serviço

--- src/app/modules/interest-list/pages/interest-list-page/interest-list-page.component.ts
+++ src/app/modules/interest-list/pages/interest-list-page/interest-list-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RandomUserService } from 'src/app/core/services/random-user.service';
+import { Interest } from 'src/app/shared/models/interest';
+
 @Component({
   selector: 'app-interest-list-page',
   templateUrl: './interest-list-page.component.html',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterestListPageComponent implements OnInit {
 
-  constructor() { }
+  users: Interest[] = [];
+
+  constructor(private randomUserService: RandomUserService) { }
 
   ngOnInit() {
+    this.randomUserService.getList()
+      .subscribe((dados: Interest[]) => {
+        this.users = dados;
+      });
   }
 
 }
