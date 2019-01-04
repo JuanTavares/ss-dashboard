45be9faff0fecee19ba291ece0c6026b33be22bd
Juan Tavares <juantavares2006@hotmail.com>
2019-01-04 00:18:24 -0300

    Mostrando usuários na tela

--- src/app/modules/users/pages/users-page/users-page.component.ts
+++ src/app/modules/users/pages/users-page/users-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RandomUserService } from 'src/app/core/services/random-user.service';
+import { Users } from 'src/app/core/models/users';
+import { Random } from 'src/app/core/models/random';
+
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersPageComponent implements OnInit {
 
-  constructor() { }
+  users: Users[] = [];
+  selectedUser: Users;
+
+  constructor(private randomUser: RandomUserService) { }
 
   ngOnInit() {
+    this.randomUser.getHundredUsers()
+      .subscribe((dados: Random) => {
+        dados.results.map((user) => {
+          this.users.push(user);
+        });
+      });
+  }
   }
 
 }
